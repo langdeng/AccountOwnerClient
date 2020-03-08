7663ef7c1dc46dac27dc202f26761ea57c08cc00
Lang <langdeng@hotmail.com>
2020-03-08 01:44:01 -0800

    cleanup

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorModalComponent } from './modals/error-modal/error-modal.component';
 import { SuccessModalComponent } from './modals/success-modal/success-modal.component';
+import { DatepickerDirective } from './directives/datepicker.directive';
 
 @NgModule({
   declarations: [
     ErrorModalComponent,
     SuccessModalComponent,
+    DatepickerDirective
   ],
   imports: [
-    CommonModule
+    CommonModule,
   ],
   exports: [ 
     ErrorModalComponent,
     SuccessModalComponent,
+    DatepickerDirective
   ]
 })
 export class SharedModule { }
